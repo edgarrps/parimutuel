0b29d85adf5c2e206c0bae00150db385d36702de
edgarrps <edgarrps@gmail.com>
2023-05-09 23:17:32 -0400

    Footer added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <div className="absolute flex justify-center items-center bottom-0 w-full h-80 bg-blue-400">
+            <section>
+                <p className="font-bold">Parimutuel Project - 2023</p>
+            </section>
+        </div>
+    )
+}
