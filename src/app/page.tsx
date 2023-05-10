701a648f1105364fdecf292c3d21e67c4838d64c
edgarrps <edgarrps@gmail.com>
2023-05-10 01:15:37 -0400

    text intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ export default function Home() {
   return (
     <>
       <Header />
+      <div className="pt-10">
+        <p className="text-center text-8xl font-bold">Parimutuel Project - New way to engage to crypto parimutuel</p>
+      </div>
       <Footer />
     </>
   )
